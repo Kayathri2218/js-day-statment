5b43368e618c0996bf2487d3436f6a9ba4b859b9
Kayathri2218 <kaya3kayu2000@gmail.com>
2023-02-15 01:58:50 +0530

    adding javascript file in day.

---
+++ main.js
@@ -0,0 +1,18 @@
+
+let day = prompt("Enter day");
+
+if (day == "monday") {
+    console.log("Working Day");
+} else if (day== "tuesday") {
+    console.log("Working Day");
+} else if (day == "wednesday") {
+    console.log("Working Day");
+} else if (day == "thursday") {
+    console.log("Working Day");
+} else if (day == "Friday"){
+    console.log("Working Day");
+}else if (day == "saturday"){
+    console.log("Working Day");
+}else if (day == "sunday"){
+    console.log("Weekend");
+}
